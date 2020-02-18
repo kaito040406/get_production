1871965a540367f22ff935c56fbf4eef25480e44
kaito hasegawa <kaitohasegawa000@gmail.com>
2020-02-18 21:43:39 +0900

    簡易マークアップ

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 import os, sys, time
 import tkinter as tk
 root = tk.Tk()
+root.title(u"Get Production")
+root.geometry("800x1000")
+root.configure(bg='#CCFFCC')
+
+Static1 = tk.Label(root, text=u'自動取得アプリ', font=(u'ＭＳ ゴシック', 25))
+Static1.place(x=320, y=10)
+Static1.config(bg='#CCFFCC')
+
+Static2 = tk.Label(root, text=u'商品検索URL', font=(u'ＭＳ ゴシック', 18))
+Static2.place(x=100, y=70)
+Static2.config(bg='#CCFFCC')
+
+EditBox2 = tk.Entry(root, width=45)
+EditBox2.place(x=230, y=70)
+
+
+Static3 = tk.Label(root, text=u'取得件数', font=(u'ＭＳ ゴシック', 18))
+Static3.place(x=100, y=110)
+Static3.config(bg='#CCFFCC')
+
+EditBox3 = tk.Entry(root, width=10)
+EditBox3.place(x=230, y=110)
+
+
+Button = tk.Button(root, text=u'実行', fg='#333333', height=2, width=10)
+Button.config(bg='red')
+Button.place(x=550, y=190)
+
 
 root.mainloop()
