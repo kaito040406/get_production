5cd14e549b26b9daf621ce013fbf28b68fab1557
kaito hasegawa <kaitohasegawa000@gmail.com>
2020-02-18 22:06:22 +0900

    簡易マークアップ２

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import os, sys, time
 import tkinter as tk
+from tkinter import messagebox
+
 root = tk.Tk()
 root.title(u"Get Production")
 root.geometry("800x1000")
 root.configure(bg='#CCFFCC')
+
+def GetValueSearchConditions(event):
+  search_url = EditBox2.get()
+  search_number = EditBox3.get()
+  try:
+    int_search_number = int(search_number)
+    print(search_url)
+    print(search_number)
+  except ValueError:
+    messagebox.showinfo('エラー', '数字を入力してください')
+    EditBox2.delete(0,tk.END)
+    EditBox3.delete(0,tk.END)
 
 Static1 = tk.Label(root, text=u'自動取得アプリ', font=(u'ＭＳ ゴシック', 25))
 Static1.place(x=320, y=10)
@@ -27,6 +41,7 @@ EditBox3.place(x=230, y=110)
 
 Button = tk.Button(root, text=u'実行', fg='#333333', height=2, width=10)
 Button.config(bg='red')
+Button.bind("<Button-1>",GetValueSearchConditions)
 Button.place(x=550, y=190)
 
 
