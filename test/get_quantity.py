39c6ea67e0153ada88563b698675a3649336b78f
kaito hasegawa <kaitohasegawa000@gmail.com>
2020-02-25 20:35:47 +0900

    在庫ありと在庫なしの判定の実装

--- test/get_quantity.py
+++ test/get_quantity.py
@@ -5,7 +5,7 @@ from requests_html import HTMLSession
 import urllib.error
 import urllib.request
 
-url = "https://www.amazon.co.jp/%E3%83%99%E3%83%93%E3%83%BC%E3%82%B9%E3%83%9E%E3%82%A4%E3%83%AB-%E3%80%90S-503%E3%80%91%E3%83%A1%E3%83%AB%E3%82%B7%E3%83%BC%E3%83%9D%E3%83%83%E3%83%88%E7%94%A8-%E9%83%A8%E5%93%81%E3%83%BB%E6%B6%88%E8%80%97%E5%93%81-%E3%83%95%E3%83%AD%E3%83%BC%E3%83%88%E3%82%BB%E3%83%83%E3%83%88/dp/B07B5TP8CN/ref=sr_1_54_sspa?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&keywords=%E9%83%A8%E5%93%81&qid=1582628496&sr=8-54-spons&psc=1&smid=A22UCQTK6U22BD&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFEMk1GVVJIOUNYR0UmZW5jcnlwdGVkSWQ9QTAwOTk5NDUxOFRDVUlLV1Q0WThUJmVuY3J5cHRlZEFkSWQ9QTNQRzlDOEVSQUhDOE4md2lkZ2V0TmFtZT1zcF9idGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl"
+url = "https://www.amazon.co.jp/%E8%85%95%E6%99%82%E8%A8%88%E4%BF%AE%E7%90%86%E3%82%BB%E3%83%83%E3%83%88-%E8%85%95%E6%99%82%E8%A8%88%E3%83%99%E3%83%AB%E3%83%88%E8%AA%BF%E6%95%B4-%E8%85%95%E6%99%82%E8%A8%88%E4%BF%AE%E7%90%86%E3%83%84%E3%83%BC%E3%83%AB-%E8%85%95%E6%99%82%E8%A8%88%E4%BF%AE%E7%90%86%E5%B7%A5%E5%85%B7%E3%82%BB%E3%83%83%E3%83%88-%E8%85%95%E6%99%82%E8%A8%88%E3%83%90%E3%83%B3%E3%83%89%E8%AA%BF%E6%95%B4/dp/B07THT2HS7/ref=sxbs_sxwds-stvp?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&cv_ct_cx=%E9%83%A8%E5%93%81&keywords=%E9%83%A8%E5%93%81&pd_rd_i=B07THT2HS7&pd_rd_r=0f06d9ef-c7bd-4bb5-aafe-f165ffeb2fa7&pd_rd_w=oI5fx&pd_rd_wg=sriVJ&pf_rd_p=e29507e3-7937-4495-82f4-55b5d5ca8047&pf_rd_r=CTWSBFKKJ8N1WG2YWHQV&psc=1&qid=1582628496"
 
 session = HTMLSession()
 r = session.get(url)
@@ -20,4 +20,17 @@ for quantity_box in quantity_boxs:
     else:
       quantity = check_word[2:4]
 
+try:
+  print(quantity)
 
+except NameError:
+  quantity_boxs2= d_soup.select(".a-size-medium.a-color-success", recursive=False)
+  for quantity_box in quantity_boxs2:
+    check_word = quantity_box.get_text().replace('\n','').replace(' ','')
+    if '在庫' in check_word:
+      quantity = check_word
+  try:
+    print(quantity)
+
+  except NameError:
+    quantity = "0"
